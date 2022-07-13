1da62aeb0a1e6dfe646be0be2801ff29d8fe62c8
BarisSahin <sahinbarisdev@gmail.com>
2022-07-13 18:20:29 +0300

    TextFields and buttons are added for user login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,7 +6,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange,
+      backgroundColor: Color.fromARGB(255, 253, 176, 61),
       //AppBar's styled title with the help of wrapper container widget
       appBar: AppBar(
         title: Container(
@@ -25,6 +25,70 @@ class LoginScreen extends StatelessWidget {
             ),
           ),
         ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 18),
+            child: Icon(
+              Icons.card_giftcard,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(200),
+            ),
+            child: Center(
+              child: FlutterLogo(
+                size: 100,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+                hintText: 'E-mail',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                hintText: 'password',
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              fixedSize: const Size(90, 50),
+              padding: const EdgeInsets.all(8),
+              textStyle: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () {},
+            child: Text('Login'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: Text('Forgot a password ?'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
