b4d51620affb5e68057156cebe978dc92e29040b
BarisSahin <sahinbarisdev@gmail.com>
2022-07-13 22:50:12 +0300

    Verification email textfield and send button is added

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -19,6 +19,75 @@ class ForgotPasswordScreen extends StatelessWidget {
           ),
         ),
       ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 25,
+          ),
+          Icon(
+            Icons.arrow_downward_rounded,
+            size: 60,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 35),
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              'We are going to send a verification email to your mail address.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  //borderRadius: BorderRadius.all(),
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                    width: 2,
+                  ),
+                ),
+                prefixIcon: Icon(
+                  Icons.mail,
+                  color: Theme.of(context).primaryColor,
+                ),
+                labelText: 'Please write your email',
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                  //color: Theme.of(context).primaryColor,
+                ),
+                hintText: 'E-mail',
+                hintStyle: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              fixedSize: const Size(90, 50),
+              padding: const EdgeInsets.all(8),
+              textStyle: TextStyle(
+                fontSize: 19,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () {},
+            child: Text('Send'),
+          ),
+        ],
+      ),
     );
   }
 }
