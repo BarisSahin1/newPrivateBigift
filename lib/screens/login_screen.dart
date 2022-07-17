670f5a5cddbb15ee1dfab902b05d08627232acf3
BarisSahin <sahinbarisdev@gmail.com>
2022-07-17 16:58:30 +0300

    Split login form as a LoginScreen widget

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bi_gift_app/screens/forgot_password_screen.dart';
+import 'package:bi_gift_app/widgets/login_form.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -9,16 +10,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,6 +39,7 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          //FlutterLogo
           Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(200),
@@ -58,88 +50,19 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  //borderRadius: BorderRadius.all(),
-                  borderSide: BorderSide(
-                    color: Colors.black,
-                    width: 2,
-                  ),
-                ),
-                prefixIcon: Icon(
-                  Icons.mail,
-                  color: Theme.of(context).primaryColor,
-                ),
-                labelText: 'Email',
-                labelStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 19,
-                  fontWeight: FontWeight.bold,
-                  //color: Theme.of(context).primaryColor,
-                ),
-                hintText: 'E-mail',
-                hintStyle: TextStyle(
-                  fontSize: 18,
-                ),
+          LoginForm(),
+          //Forgot a password button
+          Container(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(ForgotPasswordScreen.routeName);
+              },
+              child: Text(
+                'Forgot a password ?',
+                style: TextStyle(fontSize: 16),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.black,
-                    width: 2,
-                  ),
-                ),
-                prefixIcon: Icon(
-                  Icons.lock,
-                  color: Theme.of(context).primaryColor,
-                ),
-                labelText: 'Password',
-                labelStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 19,
-                  fontWeight: FontWeight.bold,
-                ),
-                hintText: 'Password',
-                hintStyle: TextStyle(fontSize: 18),
-              ),
-            ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              fixedSize: const Size(90, 50),
-              padding: const EdgeInsets.all(8),
-              textStyle: TextStyle(
-                fontSize: 19,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            onPressed: () {},
-            child: Text('Login'),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context)
-                      .pushNamed(ForgotPasswordScreen.routeName);
-                },
-                child: Text(
-                  'Forgot a password ?',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-            ],
           ),
         ],
       ),
