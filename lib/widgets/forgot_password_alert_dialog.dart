0282ed87e6987733d80ffbadc3865cf1cba350fd
BarisSahin <sahinbarisdev@gmail.com>
2022-07-19 13:42:45 +0300

    OTP field widget splitted and called

--- lib/widgets/forgot_password_alert_dialog.dart
+++ lib/widgets/forgot_password_alert_dialog.dart
@@ -1,23 +1,12 @@
-import 'package:bi_gift_app/widgets/custom_text_field.dart';
 import "package:flutter/material.dart";
 
-bool textfieldState = false;
+import 'otp_field.dart';
+
 Future myDialog(context) {
-  var mediaQuery = MediaQuery.maybeOf(context)!.size;
-
-  FocusNode nodeOne = FocusNode();
-  FocusNode nodeTwo = FocusNode();
-  FocusNode nodeThree = FocusNode();
-  FocusNode nodeFour = FocusNode();
-
-  List<bool> isFilledList = [false, false, false, false];
-  List<FocusNode> nodes = [nodeOne, nodeTwo, nodeThree, nodeFour];
-
   return showDialog(
     context: context,
     builder: (ctx) => StatefulBuilder(
       builder: (context, setState) {
-        print(textfieldState);
         return AlertDialog(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(
@@ -29,51 +18,7 @@ Future myDialog(context) {
             textAlign: TextAlign.center,
           ),
           actions: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CustomTextField(
-                  textAlign: TextAlign.center,
-                  isFilled: isFilledList[0],
-                  node: nodeOne,
-                  onChanged: (value) => setState(() => isFilledList[0] = true),
-                  height: mediaQuery.height / 20,
-                  width: mediaQuery.width / 8,
-                  inputType: TextInputType.number,
-                  lengthLimitingTextInput: 1,
-                ),
-                CustomTextField(
-                  textAlign: TextAlign.center,
-                  isFilled: isFilledList[1],
-                  node: nodeTwo,
-                  onChanged: (value) => setState(() => isFilledList[1] = true),
-                  height: mediaQuery.height / 20,
-                  width: mediaQuery.width / 8,
-                  inputType: TextInputType.number,
-                  lengthLimitingTextInput: 1,
-                ),
-                CustomTextField(
-                  textAlign: TextAlign.center,
-                  isFilled: isFilledList[2],
-                  node: nodeThree,
-                  onChanged: (value) => setState(() => isFilledList[2] = true),
-                  height: mediaQuery.height / 20,
-                  width: mediaQuery.width / 8,
-                  inputType: TextInputType.number,
-                  lengthLimitingTextInput: 1,
-                ),
-                CustomTextField(
-                  textAlign: TextAlign.center,
-                  isFilled: isFilledList[3],
-                  node: nodeFour,
-                  onChanged: (value) => setState(() => isFilledList[3] = true),
-                  height: mediaQuery.height / 20,
-                  width: mediaQuery.width / 8,
-                  inputType: TextInputType.number,
-                  lengthLimitingTextInput: 1,
-                ),
-              ],
-            ),
+            OtpField(),
             TextButton(
               onPressed: () {
                 Navigator.of(ctx).pop();
