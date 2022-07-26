5834d803fd3d65dd5204348cd4b038bf9481c2e4
BarisSahin <sahinbarisdev@gmail.com>
2022-07-26 15:27:25 +0300

    Custom textfields are used as a otp fields

--- lib/widgets/otp_field.dart
+++ lib/widgets/otp_field.dart
@@ -10,11 +10,8 @@ class OtpField extends StatefulWidget {
 class _OtpFieldState extends State<OtpField> {
   //const OtpField({Key? key}) : super(key: key);
   FocusNode nodeOne = FocusNode();
-
   FocusNode nodeTwo = FocusNode();
-
   FocusNode nodeThree = FocusNode();
-
   FocusNode nodeFour = FocusNode();
 
   List<bool> isFilledList = [false, false, false, false];
@@ -23,68 +20,75 @@ class _OtpFieldState extends State<OtpField> {
   Widget build(BuildContext context) {
     List<FocusNode> nodes = [nodeOne, nodeTwo, nodeThree, nodeFour];
     var mediaQuery = MediaQuery.maybeOf(context)!.size;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
       children: [
-        CustomTextField(
-          textAlign: TextAlign.center,
-          isFilled: isFilledList[0],
-          node: nodes[0],
-          onChanged: (value) => setState(
-            () {
-              isFilledList[0] = true;
-              FocusScope.of(context).requestFocus(nodes[1]);
-            },
-          ),
-          height: mediaQuery.height / 20,
-          width: mediaQuery.width / 8,
-          inputType: TextInputType.number,
-          lengthLimitingTextInput: 1,
+        SizedBox(
+          height: 25,
         ),
-        CustomTextField(
-          textAlign: TextAlign.center,
-          isFilled: isFilledList[1],
-          node: nodes[1],
-          onChanged: (value) => setState(
-            () {
-              isFilledList[1] = true;
-              FocusScope.of(context).requestFocus(nodes[2]);
-            },
-          ),
-          height: mediaQuery.height / 20,
-          width: mediaQuery.width / 8,
-          inputType: TextInputType.number,
-          lengthLimitingTextInput: 1,
-        ),
-        CustomTextField(
-          textAlign: TextAlign.center,
-          isFilled: isFilledList[2],
-          node: nodes[2],
-          onChanged: (value) => setState(
-            () {
-              isFilledList[2] = true;
-              FocusScope.of(context).requestFocus(nodes[3]);
-            },
-          ),
-          height: mediaQuery.height / 20,
-          width: mediaQuery.width / 8,
-          inputType: TextInputType.number,
-          lengthLimitingTextInput: 1,
-        ),
-        CustomTextField(
-          textAlign: TextAlign.center,
-          isFilled: isFilledList[3],
-          node: nodes[3],
-          onChanged: (value) => setState(
-            () {
-              isFilledList[3] = true;
-              FocusScope.of(context).requestFocus(nodes[3]);
-            },
-          ),
-          height: mediaQuery.height / 20,
-          width: mediaQuery.width / 8,
-          inputType: TextInputType.number,
-          lengthLimitingTextInput: 1,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomTextField(
+              textAlign: TextAlign.center,
+              isFilled: isFilledList[0],
+              node: nodes[0],
+              onChanged: (value) => setState(
+                () {
+                  isFilledList[0] = true;
+                  FocusScope.of(context).requestFocus(nodes[1]);
+                },
+              ),
+              height: mediaQuery.height / 20,
+              width: mediaQuery.width / 8,
+              inputType: TextInputType.number,
+              lengthLimitingTextInput: 1,
+            ),
+            CustomTextField(
+              textAlign: TextAlign.center,
+              isFilled: isFilledList[1],
+              node: nodes[1],
+              onChanged: (value) => setState(
+                () {
+                  isFilledList[1] = true;
+                  FocusScope.of(context).requestFocus(nodes[2]);
+                },
+              ),
+              height: mediaQuery.height / 20,
+              width: mediaQuery.width / 8,
+              inputType: TextInputType.number,
+              lengthLimitingTextInput: 1,
+            ),
+            CustomTextField(
+              textAlign: TextAlign.center,
+              isFilled: isFilledList[2],
+              node: nodes[2],
+              onChanged: (value) => setState(
+                () {
+                  isFilledList[2] = true;
+                  FocusScope.of(context).requestFocus(nodes[3]);
+                },
+              ),
+              height: mediaQuery.height / 20,
+              width: mediaQuery.width / 8,
+              inputType: TextInputType.number,
+              lengthLimitingTextInput: 1,
+            ),
+            CustomTextField(
+              textAlign: TextAlign.center,
+              isFilled: isFilledList[3],
+              node: nodes[3],
+              onChanged: (value) => setState(
+                () {
+                  isFilledList[3] = true;
+                  FocusScope.of(context).requestFocus(nodes[3]);
+                },
+              ),
+              height: mediaQuery.height / 20,
+              width: mediaQuery.width / 8,
+              inputType: TextInputType.number,
+              lengthLimitingTextInput: 1,
+            ),
+          ],
         ),
       ],
     );
