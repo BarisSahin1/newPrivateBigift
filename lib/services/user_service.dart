d56f31fe1f793e5b2d4dfc2e0e0c9b9f767e4fa3
BarisSahin <sahinbarisdev@gmail.com>
2022-07-26 15:40:40 +0300

    changeuserloginstatus method is defined

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,28 +1,33 @@
 import 'dart:convert';
-
-import 'package:bi_gift_app/services/login_handler.dart';
+import 'package:bi_gift_app/model/user.dart';
+import 'package:bi_gift_app/services/network_handler.dart';
 import 'package:http/http.dart' as http;
 
 class UserService {
+  static Future<void> changeUserLoginStatus() async {
+    User.isLoggedIn = true;
+  }
+
   static Future<int> userLogin(String email, String password) async {
     Map jsonData = {'email': email, 'password': password};
     //encode Map to JSON
     var body = json.encode(jsonData);
-
+    //Http post request to login
     http.Response response = await http.post(
       Uri.parse('http://10.0.2.2:3000/auth/sign_in'),
       headers: {"Content-Type": "application/json"},
       body: body,
     );
+
     Map data = jsonDecode(response.body);
     var accessToken = response.headers['access-token'];
 
     if (response.statusCode == 200) {
-      LoginHandler.storeToken(accessToken);
+      NetworkHandler.storeToken(accessToken);
       print(data);
       print(response.statusCode);
       print(accessToken);
-      print(LoginHandler.isLoggedIn);
+      print(NetworkHandler.isLoggedIn);
     } else {
       print("Error");
     }
