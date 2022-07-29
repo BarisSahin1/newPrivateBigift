8c4fa01c14d122a2dcab76c26bc871e8deaa943f
BarisSahin <sahinbarisdev@gmail.com>
2022-07-29 11:44:48 +0300

    nickname url property is added,tojson method is added

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -2,16 +2,27 @@ class User {
   String? email;
   String? password;
   String? name;
+  String? nickname;
+  String? url;
 
   User({
     this.email,
     this.password,
     this.name,
+    this.nickname,
+    this.url,
   });
 
   User.fromJson(Map<String, dynamic> json) {
-    email = json["email"];
-    password = json["password"];
-    name = json["name"];
+    email = json["data"]["email"];
+    name = json["data"]["name"];
+    nickname = json["data"]["nickname"];
   }
+
+  Map<String, dynamic> toJson() => {
+        'email': email,
+        'password': password,
+        'name': name,
+        'nickname': nickname,
+      };
 }
