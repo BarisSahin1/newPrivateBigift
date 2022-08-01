cee25598f341bb249bb84d6c5253fc6bc6a41bae
BarisSahin <sahinbarisdev@gmail.com>
2022-08-01 11:53:27 +0300

    padding and size extensions added

--- lib/extensions/media_query_extension.dart
+++ lib/extensions/media_query_extension.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
 extension MediaQueryWithValues on BuildContext {
-  double width(val) => MediaQuery.of(this).size.width / val;
-  double height(val) => MediaQuery.of(this).size.height / val;
+  double widthVal(val) => MediaQuery.of(this).size.width / val;
+  double heightVal(val) => MediaQuery.of(this).size.height / val;
 }
 
-extension MediaQueryValues on BuildContext {
-  double get widthMediaQuery => MediaQuery.of(this).size.width;
-  double get heightMediaQuery => MediaQuery.of(this).size.height;
+extension sizeExtension on BuildContext {
+  double get sizeExt => 10;
+}
+
+extension MediaQueryExtension on BuildContext {
+  double get width => MediaQuery.of(this).size.width;
+  double get height => MediaQuery.of(this).size.height;
+
+  double get lowestValue => height * 0.005;
+  double get lowValue => height * 0.01;
+  double get normalValue => height * 0.015;
+  double get mediumValue => height * 0.020;
+  double get highValue => height * 0.025;
+}
+
+extension PaddingExtension on BuildContext {
+  EdgeInsets get paddingLowest => EdgeInsets.all(lowestValue);
+  EdgeInsets get paddingLow => EdgeInsets.all(lowValue);
+  EdgeInsets get paddingNormal => EdgeInsets.all(normalValue);
+  EdgeInsets get paddingMedium => EdgeInsets.all(mediumValue);
+  EdgeInsets get paddingHigh => EdgeInsets.all(highValue);
 }
 
 // extension MediaQueryExtension on BuildContext {
